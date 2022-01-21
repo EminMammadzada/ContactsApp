a6bdb5233602dd5392160ec9b1a589958a4a3ddf
Emin Mammadzada <eminmamedzade37@gmail.com>
2022-01-21 13:16:12 -0500

    add eastern time as default time. Still needs fixes to update the last logged in field

--- LAMPAPI/login.php
+++ LAMPAPI/login.php
@@ -1,9 +1,13 @@
 <?php
 
     $inData = getRequestInfo();
+    $login = $inData['login'];
+    $password = $inData['password'];
+
     $id = 0;
     $firstName = "";
     $lastName = "";
+    
     date_default_timezone_set("America/New_York");
     $currentTime = date("Y-m-d h:i:sa");
 
@@ -14,10 +18,9 @@
 
     else{
         $stmt = $conn->prepare("SELECT ID, FirstName, LastName FROM Users WHERE Login=? AND Password=?");
-        $login = $inData['login'];
-        $password = $inData['password'];
         $stmt->bind_param("ss", $login, $password);
         $stmt->execute();
+
         $result = $stmt->get_result();
 
         if ($row = $result->fetch_assoc()){
@@ -27,10 +30,7 @@
         else{
             returnWithError("No records found");
         }
-
-        $sql = "UPDATE Users SET DateLastLoggedIn=$currentTime WHERE Login=$login";
-        $conn->query($sql);
-
+        
         $stmt->close();
 		$conn->close();
     }
@@ -51,7 +51,7 @@
 		sendResultInfoAsJson( $retValue );
 	}
 
-    function returnWithInfo( $firstName, $lastName, $id )
+    function returnWithInfo( $firstName, $lastName, $id)
 	{
 		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
 		sendResultInfoAsJson( $retValue );
