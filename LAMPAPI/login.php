d6ec6e7ff27a77786f3cc07109223c786d564cd5
Emin Mammadzada <eminmamedzade37@gmail.com>
2022-01-21 10:27:23 -0500

    modify login endpoint to update the last logged in field in database

--- LAMPAPI/login.php
+++ LAMPAPI/login.php
@@ -4,6 +4,8 @@
     $id = 0;
     $firstName = "";
     $lastName = "";
+    date_default_timezone_set("America/New_York");
+    $currentTime = date("Y-m-d h:i:sa");
 
     $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
     if ($conn->connect_error){
@@ -12,7 +14,9 @@
 
     else{
         $stmt = $conn->prepare("SELECT ID, FirstName, LastName FROM Users WHERE Login=? AND Password=?");
-        $stmt->bind_param("ss", $inData["login"], $inData["password"]);
+        $login = $inData['login'];
+        $password = $inData['password'];
+        $stmt->bind_param("ss", $login, $password);
         $stmt->execute();
         $result = $stmt->get_result();
 
@@ -23,6 +27,9 @@
         else{
             returnWithError("No records found");
         }
+
+        $sql = "UPDATE Users SET DateLastLoggedIn=$currentTime WHERE Login=$login";
+        $conn->query($sql);
 
         $stmt->close();
 		$conn->close();
