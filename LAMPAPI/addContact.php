c2ed672d68458178cbb3d1c628c17c363f19f04e
Emin Mammadzada <eminmamedzade37@gmail.com>
2022-01-22 21:30:28 -0500

    add functionality to addContact endpoint so that user does not add duplicate contacts. Duplicate in this context means the exact same FirstName, LastName, Email, and Phone

--- LAMPAPI/addContact.php
+++ LAMPAPI/addContact.php
@@ -23,11 +23,30 @@
         //check if we are adding contacts for registered users. If the user is not registered, do not create contacts for them
 
         if ($row = $result->fetch_assoc()){
-            $stmt2 = $conn->prepare("INSERT into Records (UserID,FirstName,LastName,Email,Phone) VALUES(?,?,?,?,?)");
-            $stmt2->bind_param("sssss", $userId, $firstName, $lastName, $email, $phone);
-            $stmt2->execute();
-            $stmt2->close();
-            returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID']);
+
+			//check if the user already has a contact with given credentials. If yes, then don't add repetitive field
+
+			$stmt2 = $conn->prepare("SELECT * FROM Records WHERE FirstName=? AND LastName=? AND Email=? AND Phone=? AND UserID=?");
+			$stmt2->bind_param("sssss", $firstName, $lastName, $email, $phone, $userId);
+			$stmt2->execute();
+
+			$result2 = $stmt2->get_result();
+
+
+			//there is a contact with given credentials
+			if ($row2 = $result2->fetch_assoc()){
+				returnWithError("You already have this contact");
+			}
+
+
+			//the record being added is new
+			else{
+				$stmt3 = $conn->prepare("INSERT into Records (UserID,FirstName,LastName,Email,Phone) VALUES(?,?,?,?,?)");
+				$stmt3->bind_param("sssss", $userId, $firstName, $lastName, $email, $phone);
+				$stmt3->execute();
+				$stmt3->close();
+				returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID']);
+			}
         }
 
         else{
