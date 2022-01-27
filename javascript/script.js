f4bd030af9a42c2583a9097f719819807cba2d1e
Dylan Piper <dylan_piper@knights.ucf.edu>
2022-01-26 23:57:46 -0500

    Adds form validation for registration

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,180 @@
-const form = document.getElementById('form')
-const fname = document.getElementById('fname')
-const lname = document.getElementById('lname')
-const password = document.getElementById('password')
-const errorElement = document.GetElementById('error')
+const firstEl = document.querySelector('#fname');
+const lastEl = document.querySelector('#lname');
+const usernameEl = document.querySelector('#userID');
+const passwordEl = document.querySelector('#password');
+const confirmPasswordEl = document.querySelector('#confirmpass');
 
-form.addEventListener('submit', (e) => {
-	e.preventDefault()
-	let messages = []
-	if(fname.value === '' || fname.value == null)
-	{
-		messages.push('First Name is Required.')
-	}
+const form = document.querySelector('#register');
 
-	if(messages.length > 0)
-	{
-		errorElement.innerText = messages.join(', ')
-	}
 
-})
+const checkfirst = () => {
+
+    let valid = false;
+
+    const first = firstEl.value.trim();
+
+    if (!isRequired(first)) {
+        showError(firstEl, 'First name cannot be blank.');
+    } else {
+        showSuccess(firstEl);
+        valid = true;
+    }
+    return valid;
+};
+
+const checklast = () => {
+
+    let valid = false;
+
+    const last = lastEl.value.trim();
+
+    if (!isRequired(last)) {
+        showError(lastEl, 'Last name cannot be blank.');
+    } else {
+        showSuccess(lastEl);
+        valid = true;
+    }
+    return valid;
+};
+
+const checkUsername = () => {
+
+    let valid = false;
+
+    const min = 3,
+        max = 25;
+
+    const username = usernameEl.value.trim();
+
+    if (!isRequired(username)) {
+        showError(usernameEl, 'Username cannot be blank.');
+    } else if (!isBetween(username.length, min, max)) {
+        showError(usernameEl, `Username must be between ${min} and ${max} characters.`)
+    } else {
+        showSuccess(usernameEl);
+        valid = true;
+    }
+    return valid;
+};
+
+const checkPassword = () => {
+    let valid = false;
+
+    const password = passwordEl.value.trim();
+    if (!isRequired(password)) {
+        showError(passwordEl, 'Password cannot be blank.');
+    } else {
+        showSuccess(passwordEl);
+        valid = true;
+    }
+
+    return valid;
+};
+
+const checkConfirmPassword = () => {
+    let valid = false;
+    const confirmPassword = confirmPasswordEl.value.trim();
+    const password = passwordEl.value.trim();
+
+    if (!isRequired(confirmPassword)) {
+        showError(confirmPasswordEl, 'Please enter the password again');
+    } else if (password !== confirmPassword) {
+        showError(confirmPasswordEl, 'The password does not match');
+    } else {
+        showSuccess(confirmPasswordEl);
+        valid = true;
+    }
+
+    return valid;
+};
+
+const isRequired = value => value === '' ? false : true;
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+
+const showError = (input, message) => {
+    // get the form-field element
+    const formField = input.parentElement;
+    // add the error class
+    formField.classList.remove('success');
+    formField.classList.add('error');
+
+    // show the error message
+    const error = formField.querySelector('small');
+    error.textContent = message;
+};
+
+const showSuccess = (input) => {
+    // get the form-field element
+    const formField = input.parentElement;
+
+    // remove the error class
+    formField.classList.remove('error');
+    formField.classList.add('success');
+
+    // hide the error message
+    const error = formField.querySelector('small');
+    error.textContent = '';
+}
+
+
+form.addEventListener('submit', function (e) {
+    // prevent the form from submitting
+    e.preventDefault();
+
+    // validate fields
+    let isFirstValid = checkfirst(),
+		isLastValid = checklast(),		
+		isUsernameValid = checkUsername(),
+        isEmailValid = checkEmail(),
+        isPasswordValid = checkPassword(),
+        isConfirmPasswordValid = checkConfirmPassword();
+
+    let isFormValid = isFirstValid && isLastValid && isUsernameValid &&
+        isEmailValid &&
+        isPasswordValid &&
+        isConfirmPasswordValid;
+
+    // submit to the server if the form is valid
+    if (isFormValid) {
+
+    }
+});
+
+
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        // setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay);
+    };
+};
+
+form.addEventListener('input', debounce(function (e) {
+    switch (e.target.id) {
+		case 'fname':
+            checkfirst();
+            break;
+		case 'lname':
+            checklast();
+            break;	
+        case 'username':
+            checkUsername();
+            break;
+        case 'email':
+            checkEmail();
+            break;
+        case 'password':
+            checkPassword();
+            break;
+        case 'confirmpass':
+            checkConfirmPassword();
+            break;
+    }
+}));
