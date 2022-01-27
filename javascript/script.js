41170060b4be971a46c8ae5eea7957fabff9bf7b
Dylan Piper <dylan_piper@knights.ucf.edu>
2022-01-27 02:35:36 -0500

    Fixes form validation for login and register

--- javascript/script.js
+++ javascript/script.js
@@ -1,180 +1,120 @@
-const firstEl = document.querySelector('#fname');
-const lastEl = document.querySelector('#lname');
-const usernameEl = document.querySelector('#userID');
-const passwordEl = document.querySelector('#password');
-const confirmPasswordEl = document.querySelector('#confirmpass');
+function validRegister() {
+    let firstname = document.getElementById("rfirst").value;
+    let lastname = document.getElementById("rlast").value;
+    let username = document.getElementById("ruser").value;
+    let password = document.getElementById("rpassword").value;
 
-const form = document.querySelector('#register');
+    const isRequired = value => value === '' ? false : true;
+    const isBetween = (length, min, max) => length < min || length > max ? false : true;
+    const min = 3, max = 25;
 
-
-const checkfirst = () => {
-
-    let valid = false;
-
-    const first = firstEl.value.trim();
-
-    if (!isRequired(first)) {
-        showError(firstEl, 'First name cannot be blank.');
-    } else {
-        showSuccess(firstEl);
-        valid = true;
+    if (!isRequired(firstname)) {
+        window.alert("First name cannot be blank.");
+        firstname.focus();
+        return false;
     }
-    return valid;
-};
 
-const checklast = () => {
-
-    let valid = false;
-
-    const last = lastEl.value.trim();
-
-    if (!isRequired(last)) {
-        showError(lastEl, 'Last name cannot be blank.');
-    } else {
-        showSuccess(lastEl);
-        valid = true;
+    if (!isRequired(lastname)) {
+        window.alert("Last name cannot be blank");
+        lastname.focus();
+        return false;
     }
-    return valid;
-};
-
-const checkUsername = () => {
-
-    let valid = false;
-
-    const min = 3,
-        max = 25;
-
-    const username = usernameEl.value.trim();
 
     if (!isRequired(username)) {
-        showError(usernameEl, 'Username cannot be blank.');
-    } else if (!isBetween(username.length, min, max)) {
-        showError(usernameEl, `Username must be between ${min} and ${max} characters.`)
-    } else {
-        showSuccess(usernameEl);
-        valid = true;
+        window.alert("Username cannot be blank.");
+        username.focus();
+        return false;
     }
-    return valid;
-};
 
-const checkPassword = () => {
-    let valid = false;
+    if (!isBetween(username.length, min, max)) {
+        window.alert("Username must be between 3 and 25 characters.");
+        username.focus();
+        return false;
+    }
 
-    const password = passwordEl.value.trim();
     if (!isRequired(password)) {
-        showError(passwordEl, 'Password cannot be blank.');
-    } else {
-        showSuccess(passwordEl);
-        valid = true;
+        window.alert("Password cannot be blank.");
+        password.focus();
+        return false;
     }
 
-    return valid;
-};
-
-const checkConfirmPassword = () => {
-    let valid = false;
-    const confirmPassword = confirmPasswordEl.value.trim();
-    const password = passwordEl.value.trim();
-
-    if (!isRequired(confirmPassword)) {
-        showError(confirmPasswordEl, 'Please enter the password again');
-    } else if (password !== confirmPassword) {
-        showError(confirmPasswordEl, 'The password does not match');
-    } else {
-        showSuccess(confirmPasswordEl);
-        valid = true;
-    }
-
-    return valid;
-};
-
-const isRequired = value => value === '' ? false : true;
-const isBetween = (length, min, max) => length < min || length > max ? false : true;
-
-
-const showError = (input, message) => {
-    // get the form-field element
-    const formField = input.parentElement;
-    // add the error class
-    formField.classList.remove('success');
-    formField.classList.add('error');
-
-    // show the error message
-    const error = formField.querySelector('small');
-    error.textContent = message;
-};
-
-const showSuccess = (input) => {
-    // get the form-field element
-    const formField = input.parentElement;
-
-    // remove the error class
-    formField.classList.remove('error');
-    formField.classList.add('success');
-
-    // hide the error message
-    const error = formField.querySelector('small');
-    error.textContent = '';
+    alert("Registration Form Success!")
+    register()
 }
 
+function validLogin() {
+    let username = document.getElementById("user").value;
+    let password = document.getElementById("password").value;
 
-form.addEventListener('submit', function (e) {
-    // prevent the form from submitting
-    e.preventDefault();
+    const isRequired = value => value === '' ? false : true;
+    const isBetween = (length, min, max) => length < min || length > max ? false : true;
+    const min = 3, max = 25;
 
-    // validate fields
-    let isFirstValid = checkfirst(),
-		isLastValid = checklast(),		
-		isUsernameValid = checkUsername(),
-        isEmailValid = checkEmail(),
-        isPasswordValid = checkPassword(),
-        isConfirmPasswordValid = checkConfirmPassword();
-
-    let isFormValid = isFirstValid && isLastValid && isUsernameValid &&
-        isEmailValid &&
-        isPasswordValid &&
-        isConfirmPasswordValid;
-
-    // submit to the server if the form is valid
-    if (isFormValid) {
-
+    if (!isRequired(username)) {
+        window.alert("Username cannot be blank.");
+        username.focus();
+        return false;
     }
-});
 
-
-const debounce = (fn, delay = 500) => {
-    let timeoutId;
-    return (...args) => {
-        // cancel the previous timer
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        // setup a new timer
-        timeoutId = setTimeout(() => {
-            fn.apply(null, args)
-        }, delay);
-    };
-};
-
-form.addEventListener('input', debounce(function (e) {
-    switch (e.target.id) {
-		case 'fname':
-            checkfirst();
-            break;
-		case 'lname':
-            checklast();
-            break;	
-        case 'username':
-            checkUsername();
-            break;
-        case 'email':
-            checkEmail();
-            break;
-        case 'password':
-            checkPassword();
-            break;
-        case 'confirmpass':
-            checkConfirmPassword();
-            break;
+    if (!isBetween(username.length, min, max)) {
+        window.alert("Username must be between 3 and 25 characters.");
+        username.focus();
+        return false;
     }
-}));
+
+    if (!isRequired(password)) {
+        window.alert("Password cannot be blank.");
+        password.focus();
+        return false;
+    }
+
+    login()
+}
+
+const urlBase = 'http://127.0.0.1:5500/LAMPAPI';
+const extension = 'php';
+
+function register() {
+    userID = 0;
+    let firstname = document.getElementById("rfirst").value;
+    let lastname = document.getElementById("rlast").value;
+    let username = document.getElementById("ruser").value;
+    let password = document.getElementById("rpassword").value;
+
+    document.getElementById("registerResult").innerHTML = "";
+
+    let temp = {firstname:firstname, lastname:lastname, login:username, password:password};
+    let jsonPayload = JSON.stringify( tmp );
+
+    let url = urlBase + '/register.' + extension;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    
+
+    try {
+        xhr.onreadystatechange = function() {
+            
+            if (this.readyState == 4 && this.status == 200) {
+                
+                let jsonobject = JSON.parse( xhr.responseText );
+                userID = jsonObject.ID;
+
+                firstname = jsonobject.firstname;
+                lastname = jsonobject.lastname;
+
+                window.location.href = "contact.html";
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch(err) {
+        document.getElementById("loginResult").innerHTML = err.message;
+    }
+}
+
+function login() {
+    alert("Login Form Success!")
+}
