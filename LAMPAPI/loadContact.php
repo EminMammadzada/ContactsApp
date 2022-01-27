e218a9e8ce5cfd5b12bb34b3e9738c445053bfd3
Emin Mammadzada <eminmamedzade37@gmail.com>
2022-01-27 14:29:22 -0500

    fix loadContact endpoint

--- LAMPAPI/loadContact.php
+++ LAMPAPI/loadContact.php
@@ -15,37 +15,23 @@
   // grab the user id from the session and scrape the contact info from the screen
 
   $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-  if ($conn->connect_error)
-  {
+  if ($conn->connect_error){
     returnWithError( $conn->connect_error );
   }
-  else
-  {
-    $stmt = $conn->prepare("SELECT ID FROM Records WHERE FirstName LIKE '%?%' AND LastName LIKE '%?%' AND Email LIKE '%?%' AND Phone LIKE '%?%' AND UserID=?");
+
+  else{
+    $stmt = $conn->prepare("SELECT ID FROM Records WHERE FirstName=? AND LastName=? AND Email=? AND Phone=? AND UserID=?");
     $stmt->bind_param("sssss", $firstName, $lastName, $email, $phone, $userID);
     $stmt->execute();
 
     $result = $stmt->get_result();
 
-    // FIXME: this is not in a json format \/\/\/\/\/
-    sendResultInfoAsJson($result->fetch_assoc());
+    if ($row = $result->fetch_assoc()){
+       returnWithInfo($row['ID']);
+    }
 
-    //
-    // // if there is such contact, then delete it
-    // if ($row = $result->fetch_assoc())
-    // {
-    //   $stmt2 =  $conn->prepare("UPDATE Records SET FirstName=? AND LastName=? AND Email=? AND Phone=? WHERE ID=? AND UserID=?");
-    //   $stmt2->bind_param("ssss", $firstName, $lastName, $email, $phone, $recordID, $userID);
-    //   $stmt2->execute();
-    //
-    //   returnWithInfo("");
-    //   $stmt2->close();
-    // }
-
-    // if contact does not exist, then error out
-    else
-    {
-      returnWithError("No such contact exists");
+    else{
+        returnWithError("Record not found");
     }
 
     $stmt->close();
@@ -53,6 +39,7 @@
 
   }
 
+  
   function getRequestInfo()
   {
     return json_decode(file_get_contents('php://input'), true);
